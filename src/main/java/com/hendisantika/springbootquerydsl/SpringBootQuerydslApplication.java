64159cd10d3406db82468996e56dc267cfd1a1d7
Hendi Santika <hendisantika@gmail.com>
2018-05-17 08:23:49 +0700

    #2 - Initialize Project

---
+++ src/main/java/com/hendisantika/springbootquerydsl/SpringBootQuerydslApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuerydslApplication.class, args);
+    }
+}
