e816a1b5c4712c6070765ce448a3d0621b423f8b
Hendi Santika <hendisantika@gmail.com>
2018-05-22 06:08:35 +0700

    #12 - Modify Main App

--- src/main/java/com/hendisantika/springbootquerydsl/SpringBootQuerydslApplication.java
+++ src/main/java/com/hendisantika/springbootquerydsl/SpringBootQuerydslApplication.java
@@ -1,12 +1,40 @@
 package com.hendisantika.springbootquerydsl;
 
+import com.hendisantika.springbootquerydsl.domain.Admin;
+import com.hendisantika.springbootquerydsl.domain.User;
+import com.hendisantika.springbootquerydsl.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
+@Slf4j
 @SpringBootApplication
-public class SpringBootQuerydslApplication {
+public class SpringBootQuerydslApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootQuerydslApplication.class, args);
+    }
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void run(String... strings) {
+        // Test code
+
+        List<Admin> admins = userService.
+                findAdminByUsernameAndEmail("steve_jobs", "sjobs@apple.com");
+
+        List<User> users = userService.
+                findUsersByUsernameAndEmail("jeff_bezos", "jbezos@amazon.com");
+
+
+        log.info("{}", admins.toString());
+        log.info("{}", users.toString());
+
     }
 }
